ac2a48315031f42ed7427e41f9dfbb836c350219
Dardo Colato <dardo.camano@eest3necochea.edu.ar>
2024-04-08 19:49:21 -0300

    last

---
+++ configDB.php
@@ -0,0 +1,5 @@
+<?php
+const DBM_NAME = 'inet';
+const DBM_HOST = '192.168.0.251';
+const DBM_USER = 'usuario';
+const DBM_PASSWORD = '123456';
--- php/connDB.php
+++ php/connDB.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
 
-const DBM_NAME = 'inet';
-const DBM_HOST = '192.168.0.251';
-const DBM_USER = 'usuario';
-const DBM_PASSWORD = '123456';
+require_once('../configDB.php');
             
 $conn = mysqli_connect(DBM_HOST, DBM_USER, DBM_PASSWORD, DBM_NAME);
 
